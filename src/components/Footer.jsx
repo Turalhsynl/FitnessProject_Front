b1a31c95cd2846ed05882654704328320e167a47
Turalhsynl <tural.huseynli.2005@gmail.com>
2025-03-24 22:26:29 +0400

    Cart page created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="p-6 bg-dark text-center">
+      <p>© 2025 FitGym. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
