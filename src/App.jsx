878bfa656c562091608a714fda080fc3b65c68d9
Turalhsynl <tural.huseynli.2005@gmail.com>
2025-03-26 02:16:29 +0400

    register and login page tokens completed and all token proccess completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,71 @@
+// import React from "react";
+// import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+// import Home from './pages/homepage/Home';
+// import Header from "./components/Header"
+// import Footer from "./components/Footer"
+// import Contact from "./pages/contactpage/Contact";
+// import Shop from "./pages/shoppage/Shop"
+// import Cart from "./pages/cartpage/Cart";
+// import Classes from "./pages/classespage/Classes";
+// import Login from "./pages/loginpage/Login";
+// import Register from "./pages/registerPage/Register";
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Header/>
+//       <Routes>
+//         <Route path="/" element={<Home/>}/>
+//         <Route path="/contact" element={<Contact/>}/>
+//         <Route path="/shop" element={<Shop/>}/>
+//         <Route path="/cart" element={<Cart/>}/>
+//         <Route path="/classes" element={<Classes/>}/>
+//         <Route path="/login" element={<Login/>}/>
+//         <Route path="/register" element={<Register/>}/>
+//       </Routes>
+//       <Footer/>
+//     </Router>
+ 
+//   );
+// }
+
+// export default App
+
 import React from "react";
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
-import Home from './pages/homepage/Home';
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { useStore } from "zustand";
+import { useAuthStore } from "./common/Store";
+import Home from "./pages/homepage/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Contact from "./pages/contactpage/Contact";
-import Shop from "./pages/shoppage/Shop"
+import Shop from "./pages/shoppage/Shop";
 import Cart from "./pages/cartpage/Cart";
 import Classes from "./pages/classespage/Classes";
 import Login from "./pages/loginpage/Login";
-// import Register from "./pages/registerPage/Register";
+import Register from "./pages/registerPage/Register";
 
 const App = () => {
+  const { accessToken } = useStore(useAuthStore);
+
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/shop" element={<Shop/>}/>
-        <Route path="/cart" element={<Cart/>}/>
-        <Route path="/classes" element={<Classes/>}/>
-        <Route path="/login" element={<Login/>}/>
-        {/* <Route path="/register" element={<Register/>}/> */}
-      </Routes>
-      <Footer/>
-    </Router>
- 
-  );
-}
+        {/* Hər kəs üçün açıq səhifələr */}
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-export default App
+        {/* Token tələb edən səhifələr */}
+        <Route path="/shop" element={accessToken ? <Shop /> : <Navigate to="/login" replace />} />
+        <Route path="/cart" element={accessToken ? <Cart /> : <Navigate to="/login" replace />} />
+        <Route path="/classes" element={accessToken ? <Classes /> : <Navigate to="/login" replace />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
