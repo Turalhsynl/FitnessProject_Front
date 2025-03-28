f8b77f2f3b363e4c0ee6887a4d65175c5b0b3a14
USER <USER@DESKTOP-JI95IOS>
2025-03-29 00:09:17 +0400

    Header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -89,40 +89,7 @@ const Header = () => {
         <h1 className="text-white text-4xl font-extrabold italic">
           <span className="text-purple-500">FIT</span>GYM
         </h1>
-
-     <div className="flex items-center space-x-6 ml-6">
-      <FaSearch className="text-white text-2xl cursor-pointer hover:text-purple-500" />
-       <button onClick={() => setIsCartOpen(true)}>
-       <FaShoppingCart className="text-white text-2xl cursor-pointer hover:text-purple-500" />
-       </button>
-      <button className="hidden sm:block bg-purple-600 text-white px-6 py-2 rounded-lg font-semibold hover:bg-purple-700 transition-all">
-      Join Us
-      </button>
-     <button onClick={toggleMenu} className="sm:hidden text-white text-2xl relative">
-       <FaBars className={`transition-transform duration-300 ${isMenuOpen ? 'rotate-90' : ''}`} />
-     </button>
-    </div>
-
-      
-     {isCartOpen && (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black/70 z-50">
-          <div className="bg-white w-full max-w-4xl p-20 rounded-xl shadow-2xl relative">
-            <button className="absolute top-4 right-6 text-gray-500 text-6xl" onClick={() => setIsCartOpen(false)}>
-              &times;
-            </button>
-            <p className="text-2xl font-semibold text-center">
-              You have <span className="font-bold">{cartItems}</span> items in your cart
-            </p>
-            <div className="flex justify-center mt-20">
-              <Link to="/shop" className="bg-black text-white px-6 py-4 rounded-md font-semibold hover:bg-gray-800" onClick={() => setIsCartOpen(false)}>
-                Go to Shopping
-              </Link>
-            </div>
-          </div>
-        </div>
-      )}
-
-    </div>
+        
     <nav className="hidden sm:flex sm:space-x-8 md:space-x-10 text-lg font-medium ml-auto">
       <Link to="/" className="relative hover:text-purple-500 after:block after:h-1 after:bg-purple-500 after:w-full after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100">Home</Link>
       <Link to="/about" className="relative hover:text-purple-500 after:block after:h-1 after:bg-purple-500 after:w-full after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100">About Us</Link>
@@ -131,7 +98,12 @@ const Header = () => {
       <Link to="/shop" className="relative hover:text-purple-500 after:block after:h-1 after:bg-purple-500 after:w-full after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100">Shop</Link>
     </nav>
 
-      {accessToken ? (
+     <div className="flex items-center space-x-6 ml-6">
+      <FaSearch className="text-white text-2xl cursor-pointer hover:text-purple-500" />
+       <button onClick={() => setIsCartOpen(true)}>
+       <FaShoppingCart className="text-white text-2xl cursor-pointer hover:text-purple-500" />
+       </button>
+       {accessToken ? (
         <div className="relative">
           <FaUserCircle
             className="text-3xl cursor-pointer"
@@ -158,16 +130,36 @@ const Header = () => {
           )}
         </div>
       ) : (
-        <Link to="/login" className="hidden sm:block bg-[#5E3AD4] text-dark px-5 py-1">
-          Join Our Club
-        </Link>
+        <Link to="/login" className="hidden sm:block bg-purple-600 text-white px-6 py-2 rounded-lg font-semibold hover:bg-purple-700 transition-all">
+        Join Our Club
+      </Link>
       )}
 
-      <div className="sm:hidden flex items-center">
-        <button onClick={toggleMenu} className="text-white">
-          <i className={`fas fa-bars text-2xl ${isMenuOpen ? "rotate-90" : ""} transition-transform duration-300`}></i>
-        </button>
+     <button onClick={toggleMenu} className="sm:hidden text-white text-2xl relative">
+       <FaBars className={`transition-transform duration-300 ${isMenuOpen ? 'rotate-90' : ''}`} />
+     </button>
+    </div>
+    </div>
+        {isCartOpen && (
+      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black/70 z-50">
+        <div className="bg-white w-full max-w-4xl p-20 rounded-xl shadow-2xl relative">
+          <button className="absolute top-4 right-6 text-gray-500 text-6xl" onClick={() => setIsCartOpen(false)}>
+            &times;
+          </button>
+          <p className="text-2xl font-semibold text-center">
+            You have <span className="font-bold">{cartItems}</span> items in your cart
+          </p>
+          <div className="flex justify-center mt-20">
+            <Link to="/shop" className="bg-black text-white px-6 py-4 rounded-md font-semibold hover:bg-gray-800" onClick={() => setIsCartOpen(false)}>
+              Go to Shopping
+            </Link>
+          </div>
+        </div>
       </div>
+    )}
+
+
+    
 
    <div className={`sm:hidden fixed top-0 left-0 w-full h-full bg-black text-white text-center transition-all duration-300 transform ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
        <nav className="space-y-6 py-20">
